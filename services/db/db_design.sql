1f8422762c3c9e1cc7bf01e970cda755fa076288
ghouse <mahammad.ghouse.rjp@gmail.com>
2019-10-20 21:15:22 +0530

    updated django models related to database

--- services/db/db_design.sql
+++ services/db/db_design.sql
@@ -1,61 +1,86 @@
+CREATE TABLE category (
+  "id" SERIAL NOT NULL PRIMARY KEY,
+  "full_name" varchar(50),
+  "created_at" timestamp,
+  UNIQUE(id)
+);
+
 CREATE TABLE herb (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
 CREATE TABLE leaf (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
 CREATE TABLE cucumber_capsicum (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
 CREATE TABLE cabbiage_califlower (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
 CREATE TABLE potato_onion_tomato (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
 CREATE TABLE root_vegitables (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
 CREATE TABLE beans_brinjal (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
   "category_id" INTEGER REFERENCES category(id),
-  "created_at" timestamp
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
-CREATE TABLE activity (
+CREATE TABLE orders (
   "id" SERIAL PRIMARY KEY,
-  "full_name" varchar(50),
+  "vegitable_name" varchar(50),
   "price" INT,
-  "created_at" timestamp
+  "vegitable_id" INT,
+  "vegitable_type" varchar(50),
+  "vegitable_category" varchar(50),
+  "status" INT,
+  "created_at" timestamp,
+  UNIQUE(id)
 );
 
-CREATE TABLE category (
-  "id" SERIAL NOT NULL PRIMARY KEY,
-  "full_name" varchar(50),
-  "created_at" timestamp
+CREATE TABLE orders_cancelled (
+  "id" SERIAL PRIMARY KEY,
+  "vegitable_name" varchar(50),
+  "price" INT,
+  "vegitable_id" INT,
+  "vegitable_type" varchar(50),
+  "vegitable_category" varchar(50),
+  "status" INT,
+  "created_at" timestamp,
+  UNIQUE(id)
 );
--- services/homeapp/api/models.py
+++ services/homeapp/api/models.py
@@ -1,28 +1,107 @@
 from django.db import models
 
 # Create your models here.
-class Herb(models.Model):
+
+class Category(models.Model):
     full_name = models.CharField(max_length=50)
     created_at = models.DateTimeField()
 
     class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
+
+
+class Herb(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='herbs')
+
+    class Meta:
         db_table = 'herb'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
 
-# class Reporter(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     email = models.EmailField()
+class Leaf(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='leafs')
 
-#     def __str__(self):
-#         return "%s %s" % (self.first_name, self.last_name)
+    class Meta:
+        db_table = 'leaf'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
 
-# class Article(models.Model):
-#     headline = models.CharField(max_length=100)
-#     pub_date = models.DateField()
-#     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE,related_name='articles')
+class Cucumber_Capsicum(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='cucumbers_capsicums')
 
-#     def __str__(self):
-#         return self.headline
+    class Meta:
+        db_table = 'cucumber_capsicum'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
 
-#     class Meta:
-#         ordering = ('headline',)
+class Cabbiage_Califlower(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='cabbiages_califlowers')
+
+    class Meta:
+        db_table = 'cabbiage_califlower'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
+
+class Potato_Onion_Tomato(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='potatos_onions_tomatos')
+
+    class Meta:
+        db_table = 'potato_onion_tomato'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
+
+class Root_Vegitables(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='root_vegies')
+
+    class Meta:
+        db_table = 'root_vegitables'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
+
+class Beans_Brinjal(models.Model):
+    full_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='beans_brinjals')
+
+    class Meta:
+        db_table = 'beans_brinjal'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
+
+class orders(models.Model):
+    vegitable_name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    vegitable_id = models.IntegerField()
+    vegitable_type = models.CharField(max_length=50)
+    vegitable_category = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    status = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='orders')
+
+    class Meta:
+        db_table = 'orders'
+    
+    def __str__(self):
+        return "%s %d" %(self.full_name,self.id)
