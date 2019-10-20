3c86bcb29d10a182cf3792e4dbbd98cc5e2e31ce
ghouse <mahammad.ghouse.rjp@gmail.com>
2019-10-20 15:38:45 +0530

    updated db_design

--- services/db/db_design.sql
+++ services/db/db_design.sql
@@ -1,42 +1,49 @@
 CREATE TABLE herb (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
 CREATE TABLE leaf (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
 CREATE TABLE cucumber_capsicum (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
 CREATE TABLE cabbiage_califlower (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
 CREATE TABLE potato_onion_tomato (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
 CREATE TABLE root_vegitables (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
 CREATE TABLE beans_brinjal (
   "id" SERIAL PRIMARY KEY,
   "full_name" varchar(50),
+  "category_id" INTEGER REFERENCES category(id),
   "created_at" timestamp
 );
 
@@ -47,15 +54,8 @@ CREATE TABLE activity (
   "created_at" timestamp
 );
 
-CREATE TABLE vegitables (
+CREATE TABLE category (
   "id" SERIAL NOT NULL PRIMARY KEY,
   "full_name" varchar(50),
-  "herbs_id" INTEGER REFERENCES herb(id),
-  "leafs_id" INTEGER REFERENCES leaf(id),
-  "Cucumber_Capsicums_id" INTEGER REFERENCES cucumber_capsicum(id),
-  "Cabbiage_Califlowers_id" INTEGER REFERENCES cabbiage_califlower(id),
-  "Potato_Onion_Tomatos_id" INTEGER REFERENCES potato_onion_tomato(id),
-  "Root_Vegitables_id" INTEGER REFERENCES root_vegitables(id),
-  "Beans_Brinjals_id" INTEGER REFERENCES beans_brinjal(id),
   "created_at" timestamp
 );
--- services/homeapp/api/models.py
+++ services/homeapp/api/models.py
@@ -7,3 +7,22 @@ class Herb(models.Model):
 
     class Meta:
         db_table = 'herb'
+
+# class Reporter(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return "%s %s" % (self.first_name, self.last_name)
+
+# class Article(models.Model):
+#     headline = models.CharField(max_length=100)
+#     pub_date = models.DateField()
+#     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE,related_name='articles')
+
+#     def __str__(self):
+#         return self.headline
+
+#     class Meta:
+#         ordering = ('headline',)
